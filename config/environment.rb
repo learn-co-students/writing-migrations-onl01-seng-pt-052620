4dfda3f7cecfbc3419706a35fdeba101685e9ded
rumikoacopa <rumikorios@gmail.com>
2020-08-12 18:01:53 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,24 @@
 require 'bundler/setup'
 Bundler.require
 
+#ActiveRecord::Base.establish_connection(
+#  :adapter => "sqlite3",
+#  :database => "db/create_students.sqlite"
+#)
+#sql = <<-SQL
+#  CREATE TABLE IF NOT EXISTS artists (
+#  id INTEGER PRIMARY KEY,
+#  name TEXT,
+#  genre TEXT,
+#  age INTEGER,
+#  hometown TEXT
+#  )
+#SQL
+# 
+#ActiveRecord::Base.connection.execute(sql)
+#require_relative "../create_students.rb"
+
+
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
 Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
 
